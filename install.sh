078bf26c26467512c6d08db8462fc34c53f13dff
Nathan West <nathan.west@datadoghq.com>
2025-11-07 17:25:39 -0500

    Use a cargo_target_dir

--- install.sh
+++ install.sh
@@ -2,16 +2,19 @@
 
 set -exu
 
+export HOME="/home/bits"
+
 export PATH="$HOME/.cargo/bin:$PATH"
 
-export APT_PACKAGES="fzf neovim stow"
-export CARGO_PACKAGES="bat bottom broot du-dust eza fd-find hurl hyperfine jj-cli procs ripgrep sd starship zellij zoxide"
+export INSTALL_APT_PACKAGES="fzf neovim stow"
+export INSTALL_CARGO_PACKAGES="bat bottom du-dust eza fd-find hurl hyperfine jj-cli procs ripgrep sd starship zellij zoxide"
+export CARGO_TARGET_DIR="$HOME/.local/state/cargo-dotfiles/target"
 
 (
   set -exu
 
   sudo apt update --yes
-  sudo apt install --yes $APT_PACKAGES
+  sudo apt install --yes $INSTALL_APT_PACKAGES
   sudo apt upgrade --yes
 
   stow --target "$HOME/.config" --dir "$HOME/dotfiles" config
@@ -19,9 +22,10 @@ export CARGO_PACKAGES="bat bottom broot du-dust eza fd-find hurl hyperfine jj-cl
 
 (
   set -exu
-  
+
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain stable --no-modify-path -y
-  cargo install --locked $CARGO_PACKAGES
+  mkdir -p "$CARGO_TARGET_DIR"
+  cargo install --locked $INSTALL_CARGO_PACKAGES
 ) &
 
 wait
