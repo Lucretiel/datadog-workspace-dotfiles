b5ace62fe390a0d3b7d16f323d5aa95a9b722252
Nathan West <nathan.west@datadoghq.com>
2025-11-07 17:12:39 -0500

    Rewrite install script for simplicity

--- install.sh
+++ install.sh
@@ -2,4 +2,26 @@
 
 set -exu
 
-exec make -j -f ~/dotfiles/INSTALL.mak all
+export PATH="$HOME/.cargo/bin:$PATH"
+
+export APT_PACKAGES="fzf neovim stow"
+export CARGO_PACKAGES="bat bottom broot du-dust eza fd-find hurl hyperfine jj-cli procs ripgrep sd starship zellij zoxide"
+
+(
+  set -exu
+
+  sudo apt update --yes
+  sudo apt install --yes $APT_PACKAGES
+  sudo apt upgrade --yes
+
+  stow --target "$HOME/.config" --dir "$HOME/dotfiles" config
+) &
+
+(
+  set -exu
+  
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain stable --no-modify-path -y
+  cargo install --locked $CARGO_PACKAGES
+) &
+
+wait
