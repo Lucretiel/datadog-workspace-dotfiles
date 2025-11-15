6c886fdff6ad770ef342d76a0ca6aeaf36206948
Nathan West <nathan.west@datadoghq.com>
2025-11-15 02:30:33 -0500

    Add SSH_AUTH_SOCK

--- variables.fish
+++ variables.fish
@@ -2,5 +2,6 @@ set -Ux EDITOR nvim
 set -Ux LESS iMR
 set -Ux STARSHIP_SHELL fish
 set -Ux EZA_ICONS_AUTO 1
+set -Ux SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
 set -U fish_user_paths ~/.local/bin ~/.cargo/bin ~/.nix-profile/bin /nix/var/nix/profiles/default/bin
 set -Ux --path XDG_DATA_DIRS ~/.local/share ~/.nix-profile/share /nix/var/nix/profiles/default/share /usr/local/share /usr/share
